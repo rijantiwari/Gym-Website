6d758992ea591d52a78ae8bb271110e451905784
Rijan Tiwari <reezantiwaree@gmail.com>
2023-10-18 22:37:31 +0545

    Right side heart image and calories amount styled

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -53,7 +53,16 @@ const Hero = () => {
           <buttons className="btn">Learn More</buttons>
         </div>
       </div>
-      <div className="right-h">Right Side</div>
+
+      {/* Right Side of the Hero  */}
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+      </div>
     </div>
   );
 };
